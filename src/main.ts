fa2b41e584f6ab37639736f01c476b30def617c2
carlos-talavera <carloseduardorivera12@gmail.com>
2025-07-18 18:41:59 -0600

    fix(app): use global validation pipe for dto

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from 'src/app.module';
@@ -5,6 +6,7 @@ import { AppModule } from 'src/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
 bootstrap();
