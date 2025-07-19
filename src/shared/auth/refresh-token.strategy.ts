e880377701a74c4fc59546cbe92109cb767f4344
carlos-talavera <carloseduardorivera12@gmail.com>
2025-07-18 19:26:23 -0600

    feat(auth): add jwt strategies and guards

---
+++ src/shared/auth/refresh-token.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_REFRESH_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  public validate(req: Request, payload: any) {
+    const authHeader = req.get('Authorization');
+    const refreshToken = authHeader
+      ? authHeader.replace('Bearer', '').trim()
+      : '';
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
