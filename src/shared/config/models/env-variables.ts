db80c67e1e5e254a3cfc712f82a3b0c003474af2
carlos-talavera <carloseduardorivera12@gmail.com>
2025-07-18 19:25:01 -0600

    build(env): include database url and jwt-related variables in the validation schema

--- src/shared/config/models/env-variables.ts
+++ src/shared/config/models/env-variables.ts
@@ -5,4 +5,9 @@ export const EnvironmentVariablesValidationSchema = Joi.object({
     .valid('development', 'production', 'staging')
     .default('development'),
   PORT: Joi.number().port().default(5000),
+  DATABASE_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRATION: Joi.string().required(),
+  JWT_REFRESH_SECRET: Joi.string().required(),
+  JWT_REFRESH_EXPIRATION: Joi.string().required(),
 });
