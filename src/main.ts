e9712fd6fa2a720169f1fcef55196b5427dc9686
carlos-talavera <carloseduardorivera12@gmail.com>
2025-07-18 18:41:39 -0600

    feat(app): register the web socket adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/platform-ws';
+import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+  app.useWebSocketAdapter(new WsAdapter(app));
+  await app.listen(5000);
 }
 bootstrap();
