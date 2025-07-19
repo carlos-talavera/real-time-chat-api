051bf3cd24f8f3633e2931c1cb72561e7860d071
carlos-talavera <carloseduardorivera12@gmail.com>
2025-07-19 14:06:19 -0600

    fix(app): remove websocket adapter from app entrypoint
    It's not needed anymore since we're using socket.io directly.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
